81d75d73bd7c34eec7af545f0db96d9075f0fd12
mechztcc <dojathan@gmail.com>
2024-01-10 14:13:25 -0500

    fix: improviments on auth decode token

--- src/middlewares/auth-decode/auth-decode.middleware.ts
+++ src/middlewares/auth-decode/auth-decode.middleware.ts
@@ -10,6 +10,9 @@ export class AuthDecodeMiddleware implements NestMiddleware {
 
   async use(req: any, res: any, next: () => void) {
     const token = req.headers['authorization'];
+    if (!token) {
+      next();
+    }
 
     if (token) {
       this.httpService
@@ -23,10 +26,9 @@ export class AuthDecodeMiddleware implements NestMiddleware {
         )
         .subscribe(({ data }: any) => {
           const { id } = data;
-          req.user_id = id;
+          req.headers.user_id = id;
+          next();
         });
     }
-
-    next();
   }
 }
